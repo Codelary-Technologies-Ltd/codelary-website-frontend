63160beb46546a4c14762821f850cb50d49e0f87
Gabbie99 <khoinom2018@gmail.com>
2022-11-01 14:30:19 +0100

    chore:routing-i

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -15,8 +15,8 @@ function HomePage() {
                 <p>We take our time to design, build and develop scalable software applications for people all over the world to solve their problems</p>
             </div>
             <div className='custom-data'>
-            <CustomButton title = 'Free Consultation' style = {{margin: '3px 0 0', border: '1px solid #AB7E5F', backgroundColor: 'transparent'}} />
-            <CustomButton title = 'Learn More' style = {{ margin: '3px 0 0', border: '1px solid #AB7E5F', backgroundColor: 'transparent', paddingLeft: '1.8rem', paddingRight: '1.8rem'}}/>
+            <a href="#contact-us"><CustomButton title = 'Free Consultation' style = {{margin: '3px 0 0', border: '1px solid #AB7E5F', backgroundColor: 'transparent'}} /></a>
+            <a href="#our-services-us"><CustomButton title = 'Learn More' style = {{ margin: '3px 0 0', border: '1px solid #AB7E5F', backgroundColor: 'transparent', paddingLeft: '1.8rem', paddingRight: '1.8rem'}}/></a>
             </div>
       </div>
       </div>
