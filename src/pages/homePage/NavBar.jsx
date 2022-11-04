013dfe479205370c22f1a02494e15dcf07514dca
Hameedarh <ajibadeolayimika@gmail.com>
2022-11-04 13:30:40 +0000

    chore:navbar styling

--- src/pages/homePage/NavBar.jsx
+++ src/pages/homePage/NavBar.jsx
@@ -6,7 +6,7 @@ function NavBar() {
 
   const [color, setColor] = useState(false)
   const colorChange = ()=>{
-    if(window.scrollY > 730){
+    if(window.scrollY > 520){
       setColor(true)
     }
     else{
