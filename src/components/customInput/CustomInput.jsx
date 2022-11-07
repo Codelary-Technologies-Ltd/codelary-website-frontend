745069c84fdb074bcbbe31c52a33764548ed7e70
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-11-07 14:22:47 +0100

    feat: added the email js features

--- src/components/customInput/CustomInput.jsx
+++ src/components/customInput/CustomInput.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function CustomInput({placeholder, type, ...otherProps}) {
   return (
     <div>
-        <input {...otherProps} placeholder={placeholder} type={type} className=" w-1/2 p-2 border-2 border-yellow-700 "/>
+        <input {...otherProps} placeholder={placeholder} required type={type} className=" w-1/2 p-2 border-2 border-yellow-700 "/>
     </div>
   )
 }
