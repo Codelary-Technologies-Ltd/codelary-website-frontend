93df7822d6ab9ea43ba486bf31a40f664be5db83
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-11-08 14:31:33 +0100

    chore: done with cleaning the outTeam page

--- src/pages/ourTeam/TeamPage.jsx
+++ src/pages/ourTeam/TeamPage.jsx
@@ -8,19 +8,14 @@ import users from './users.json';
 function TeamPage() {
   return (
     <div className='ab'>
-
         <div className='abouthead' id='our-team-us'>
-        <h1 >Our Team</h1>
-        <p>We are a team of dedicated software designers and developers. We live and breathe tech 7 days a week.</p>
+          <h1 >Our Team</h1>
+          <p>We are a team of dedicated software designers and developers. We live and breathe tech 7 days a week.</p>
         </div>
         <div className='top'>
           <TeamList userss={users}/> 
         </div>
-           
     </div>
-    
-    
-    
   )
 }
 
