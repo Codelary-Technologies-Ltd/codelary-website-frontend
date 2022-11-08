c94703b2e97e92d6f1a99726b134c30cbc3f058e
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-11-08 12:42:15 +0100

    done with the first section

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -7,16 +7,39 @@ import CustomButton from '../../components/customButton/CustomButton';
 function HomePage() {
   return (
     <div className='bod1' id='homepage-us'>
-        {/* <NavBar /> */}
-        <div className='bod'>
-            <div className='home-div'>
-                <h1 >Reliable, Secured and Customer Satisfaction for Every Client</h1>
-                <p>We take our time to design, build and develop scalable software applications for people all over the world to solve their problems</p>
-            </div>
-            <div className='custom-data'>
-            <a href="#contact-us"><CustomButton title = 'Free Consultation' style = {{margin: '3px 0 0', border: '1px solid #AB7E5F', backgroundColor: 'transparent'}} /></a>
-            <a href="#our-services-us"><CustomButton title = 'Learn More' style = {{ margin: '3px 0 0', border: '1px solid #AB7E5F', backgroundColor: 'transparent', paddingLeft: '1.8rem', paddingRight: '1.8rem'}}/></a>
-            </div>
+      <div className='bod'>
+        <div className='home-div'>
+          <h1 >
+            Reliable, Secured and Customer Satisfaction for Every Client
+          </h1>
+          <p>
+            We take our time to design, build and develop scalable software applications for people all over the world to solve their problems
+          </p>
+        </div>
+        <div className='custom-data'>
+          <a href="#contact-us">
+            <CustomButton title = 'Free Consultation' 
+            style = {{
+              margin: '3px 0 0', 
+              border: '1px solid #AB7E5F', 
+              backgroundColor: 'transparent', 
+              borderRadius:"8px",
+              padding:"5px 5px"
+            }} />
+          </a>
+          <a href="#our-services-us">
+            <CustomButton title = 'Learn More' 
+            style = {{ 
+              margin: '3px 0 0', 
+              border: '1px solid #AB7E5F', 
+              backgroundColor: 'transparent', 
+              paddingLeft: '1.8rem', 
+              paddingRight: '1.8rem', 
+              borderRadius:"8px",
+              padding:"5px 10px"
+            }}/>
+          </a>
+        </div>
       </div>
     </div>
   )
