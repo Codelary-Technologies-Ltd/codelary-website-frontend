1ac59f1044b3878b9ee16e4e663b399d1b0723f3
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-11-14 15:14:16 +0100

    chore: changed the router to browserRouter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import './App.css';
 import AllPages from './pages/allPages/AllPages';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Privacy from './pages/privacyPage/Privacy';
 
 function App() {
   return (
     <div className='App'>
-      <Router>
+      <BrowserRouter>
         <Routes>
           <Route path='/' element={<AllPages />}/>
           <Route path='/privacy-policy' element={<Privacy />}/>
           {/* <Route exact path='/services' element={<OurServices />}/> */}
         </Routes>
-      </Router>
-      
+      </BrowserRouter>
       
     </div>
   );
