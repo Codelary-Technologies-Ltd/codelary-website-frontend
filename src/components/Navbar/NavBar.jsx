17517b9563c1b15265e7ce022312c8dc28aa019e
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-11-15 21:52:45 +0100

    chore: created new navbar

---
+++ src/components/Navbar/NavBar.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import logo from '../../assets/codelary4.png';
+import Hamburger from '../Hamburger';
+import './NavBar.css'
+function NavBar() {
+
+  const [color, setColor] = useState(false)
+  const colorChange = ()=>{
+    // if(window.scrollY > 520){
+    //   setColor(true)
+    // }
+    // else{
+    //   setColor(false)
+    // }
+    window.scrollY > 520 ? setColor(true) : setColor(false)
+  }
+
+
+  window.addEventListener('scroll', colorChange)
+
+
+  return (
+      <div className={color ? 'first-nav first-nav-bg ': 'first-nav'}>
+        <div className='nav-flex'>
+            <a href="#homepage-us">
+              <img src={logo} alt="codelary-logo" className='codelary-logo' />
+              <p className='logo-txt'>odelary</p>
+            </a>
+        </div>            
+        <nav className='second-nav'>
+          <ul>
+            <li>
+              <a href="#homepage-us"> Home</a>
+            </li>
+            <li>
+              <a href="#about-us"> About Us</a>
+            </li>
+            <li>
+              <a href="#our-services-us"> Our Services</a>
+            </li>
+            <li>
+              <a href="#our-team-us"> Our Team</a>
+            </li>
+            <li>
+              <a href="#contact-us"> Contact Us</a>
+            </li>
+          </ul> 
+        </nav>
+        <div className='my-ham'>
+          <Hamburger/>
+        </div>
+      </div>
+  )
+}
+
+export default NavBar;
