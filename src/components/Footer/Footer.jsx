f7f8a960808c5e30bfdc5ef92576a38d44c1ea48
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-11-16 11:22:41 +0100

    chore:added the new footer component and ue them for term and privacy

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './Footer.css'
+import { FacebookLogo, LinkedinLogo, InstagramLogo, TwitterLogo} from 'phosphor-react';
+import { Link } from 'react-router-dom'
+
+function Footer() {
+  return (
+    <div>
+        <div className="lastpage">
+            {/* <div className="lastpage2">
+                <div className="groupc1">
+                    <a href="https://www.facebook.com/profile.php?id=100087274729762"><p><FacebookLogo size={30} className = 'bluei' /></p></a>
+                    <a href="https://www.linkedin.com/in/codelaryTech"><p><LinkedinLogo size={30} className = 'linkd' /></p></a>
+                    <a href="https://www.instagram.com"><p><InstagramLogo size={30} className = 'insta'  /></p></a>
+                    <a href="https://www.twitter.com/codelaryTech"><p><TwitterLogo size={30} className = 'twit' /></p></a>
+                </div>
+            </div> */}
+            <div className="lastpage1">
+                <Link to={'/privacy-policy'}>
+                    <span>Privacy Policy </span>
+                </Link>
+                <p>|</p>
+                <Link to={'/terms-of-use'}>
+                    <span>Terms And Conditions</span>
+                </Link>
+            </div>
+            <div className="lastpage2">
+                    <div className="groupc2">
+                        <a href="https://www.facebook.com/profile.php?id=100087274729762"><p><FacebookLogo size={30} className = 'bluei' /></p></a>
+                        <a href="https://www.linkedin.com/in/codelaryTech"><p><LinkedinLogo size={30} className = 'linkd' /></p></a>
+                        <a href="https://www.instagram.com"><p><InstagramLogo size={30} className = 'insta'  /></p></a>
+                        <a href="https://www.twitter.com/codelaryTech"><p><TwitterLogo size={30} className = 'twit' /></p></a>
+                    </div>
+            </div>
+            <div className="lastpage3">
+                <span>&copy; 2022 Codelary Technologies Ltd. All Rights Reserved</span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
--- src/pages/termsPage/Terms.jsx
+++ src/pages/termsPage/Terms.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Terms.css'
 import NavBar from '../../components/Navbar/NavBar';
-import Footer from '../footer/Footer';
+import Footer from '../../components/Footer/Footer';
 
 function Terms() {
   return (
