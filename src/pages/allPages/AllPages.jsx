b42a169aa4dde1a7d8989ad3797fea61e7b41970
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2023-08-05 20:01:29 +0100

    chore: removed the the error

--- src/pages/allPages/AllPages.jsx
+++ src/pages/allPages/AllPages.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import HomePage from '../../pages/homePage/HomePage';
 import AboutPage from '../../pages/aboutUs/AboutPage';
 import OurServices from '../../pages/ourServices/OurServices';
-import TeamPage from '../../pages/ourTeam/TeamPage';
+// import TeamPage from '../../pages/ourTeam/TeamPage';
 import ContactPage from '../../pages/contactPage/ContactPage';
 import NavBar from '../../pages/homePage/NavBar';
 
